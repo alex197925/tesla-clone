21e9df0df8d4e716463d6bf3c1e5f5751a650377
Oleksandr Tolok <tolok.oleksandr@gmail.com>
2021-05-31 17:43:58 +0200

    ADD: Section part

---
+++ src/components/Section.js
@@ -0,0 +1,83 @@
+/** @format */
+
+import React from "react";
+import styled from "styled-components";
+
+function Section({
+  title,
+  description,
+  backgroundImage,
+  leftBtnText,
+  rightBtnText,
+}) {
+  return (
+    <Wrap bgImage={backgroundImage}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src='/images/down-arrow.svg' />
+      </Buttons>
+    </Wrap>
+  );
+}
+
+export default Section;
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  margin: 8px;
+`;
+const RightButton = styled(LeftButton)`
+  background-color: #fff;
+  opacity: 0.65;
+  color: #000;
+`;
+
+const DownArrow = styled.img`
+  overflow-x: hidden;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
